1a9764321983291cf0dcca2e2a1d31a2175c0656
Shao-en Ma <bm3027@columbia.edu>
2021-03-01 19:27:42 -0500

    implemented get pulse from server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://3.20.96.207:8000";
+
+//code from https://www.valentinog.com/blog/socket-react/
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    });
+  }, []);
+
+  function colorFromRate(params) {
+    if(params == 0){
+      return '#282c34';
+    }
+    else{
+      
+      return '#282c34';
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{backgroundColor: colorFromRate(response)}}>
+        {response != 0 ? response: "Error: Heartrate Unavailable"}
       </header>
     </div>
   );
